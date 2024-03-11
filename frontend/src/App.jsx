83a7ea28a8dff83eb4d16fe54d2961536a9af1c3
tejasvenu45 <tenacioustejas@gmail.com>
2024-03-11 12:35:00 +0530

    done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+     <button class="btn">Button</button>
+     <button class="btn btn-primary">One</button>
+<button class="btn btn-secondary">Two</button>
+<button class="btn btn-accent btn-outline">Three</button>
+    </>
+  )
+}
+
+export default App
